3e3f2bfbae8e0e2e77089469b61606a7cecdb082
Victory <95528141+Victory-Dev-Teck@users.noreply.github.com>
2023-03-11 09:03:09 +0700

    Update common.js

--- pricechecker-frontend/user/assets/js/myjs/common.js
+++ pricechecker-frontend/user/assets/js/myjs/common.js
@@ -5,7 +5,7 @@ const baseUserPathName = "/pricechecker-frontend/user";
 const baseClientPathName = "/pricechecker-frontend"
 const baseServerPathNameForUser = "http://localhost/pricechecker/user";
 const numOfShowProducts = 10;
-const isMockData = true;
+const isMockData = false;
 function objLength(obj){
     let i=0;
     for (let x in obj){
